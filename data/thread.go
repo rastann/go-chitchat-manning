9b158810beb1351a6712736ce5ba5cda3d45aa88
Stan <AStan@luxoft.com>
2021-08-25 17:50:46 +0300

    init

---
+++ data/thread.go
@@ -0,0 +1,38 @@
+package data
+
+import "time"
+
+type Thread struct {
+	Id        int
+	Uuid      string
+	Topic     string
+	UserId    int
+	CreatedAt time.Time
+}
+
+type Post struct {
+	Id        int
+	Uuid      string
+	Body      string
+	UserId    int
+	ThreadId  int
+	CreatedAt time.Time
+}
+
+func Threads() (threads []Thread, err error) {
+	rows, err := Db.Query("SELET id, uuid, topic, user_id, created_at FROM threads ORDER BY created_at DESC")
+
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		conv := Thread{}
+		if err = rows.Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, conv)
+	}
+	rows.Close()
+	return
+}
